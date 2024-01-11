78445baab79c79807c1a0d309f0225eac62bfe7c
dimskuyyy <dimasfauzan1712@gmail.com>
2024-01-11 16:34:04 +0700

    Feat/Routes : Setup Route for Auth

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,15 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+// $routes->get('/', 'Home::index');
+
+$routes->group('admin', ['namespace' => 'App\Controllers\Back'], static function ($routes) {
+
+    $routes->get('logout', 'Auth::logout');
+    $routes->get('login', 'Auth::login');
+    $routes->post('login-do', 'Auth::doLogin');
+
+    $routes->group('/', ['filter' => ['isLoggedIn'], 'namespace' => 'App\Controllers\Back'], static function ($routes) {
+        $routes->get('/', 'Dashboard::index');
+    });
+});
