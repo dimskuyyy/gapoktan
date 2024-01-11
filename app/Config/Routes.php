562355bafe0f638484c021fa420aa753737427f6
dimskuyyy <dimasfauzan1712@gmail.com>
2024-01-12 06:57:32 +0700

    Fix/Routes : Fixing TypeError for filter on route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,7 +13,7 @@ $routes->group('admin', ['namespace' => 'App\Controllers\Back'], static function
     $routes->get('login', 'Auth::login');
     $routes->post('login-do', 'Auth::doLogin');
 
-    $routes->group('/', ['filter' => ['isLoggedIn'], 'namespace' => 'App\Controllers\Back'], static function ($routes) {
+    $routes->group('/', ['filter' => 'isLoggedIn', 'namespace' => 'App\Controllers\Back'], static function ($routes) {
         $routes->get('/', 'Home::index');
     });
 });
