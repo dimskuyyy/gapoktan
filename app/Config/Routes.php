1ac29aae514bc0838c7213d89d17016ca6a27a77
dimskuyyy <dimasfauzan1712@gmail.com>
2024-01-11 16:53:09 +0700

    Feat/Auth : Create view auth login

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-// $routes->get('/', 'Home::index');
+$routes->get('/', 'Home::index');
 
 $routes->group('admin', ['namespace' => 'App\Controllers\Back'], static function ($routes) {
 
